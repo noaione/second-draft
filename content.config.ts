a0e9c8df132702ae68f8ac4ca92555531061e574
noaione <noaione0809@gmail.com>
2026-01-26 21:15:05 +0700

    fix: remove collections
    since right now we're doing custom one

--- content.config.ts
+++ content.config.ts
@@ -2,20 +2,5 @@ import { defineContentConfig, defineCollection } from '@nuxt/content'
 import { z } from 'zod'
 
 export default defineContentConfig({
-  collections: {
-    content: defineCollection({
-      type: 'page',
-      source: '**/*.md',
-      schema: z.object({
-        title: z.string().min(1),
-        postId: z.string(),
-        author: z.string(),
-        collectionName: z.string(),
-        collectionId: z.string(),
-        publishedAt: z.string().refine((date) => !isNaN(Date.parse(date)), {
-          message: 'publishedAt must be a valid ISO 8601 date string',
-        }),
-      }),
-    }),
-  },
+  collections: {},
 })
