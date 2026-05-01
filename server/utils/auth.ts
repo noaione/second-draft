dd69b740af5aaaf3e5d17580be4990de0e27cb35
noaione <noaione0809@gmail.com>
2026-05-01 19:43:28 +0700

    feat: make session stays for 30 days

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -17,7 +17,7 @@ const SESSION_OPTIONS = {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     sameSite: 'lax' as const,
-    maxAge: 60 * 60 * 24 * 7, // 7 days
+    maxAge: 60 * 60 * 24 * 30, // 30 days
     path: '/',
   },
 };
